c666d3055332d6536aab5d64f4427da99e082238
bigmanyeti <subediutsav10@gmail.com>
2026-07-21 23:48:14 +0545

    login page added

--- readify-frontend/src/App.tsx
+++ readify-frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import SignupPage from './pages/SignupPage';
 import GoogleSignupCompletePage from './pages/GoogleSignupCompletePage';
+import LoginPage from './pages/LoginPage';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Toaster position="top-center" toastOptions={{ duration: 3500 }} />
       <Routes>
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/signup/complete-profile" element={<GoogleSignupCompletePage />} />
         <Route path="/" element={<Navigate to="/signup" replace />} />
